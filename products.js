a332263b5f3b30186ed5c53f7b7bffb32a720f63
genez75016 <genez75016@gmail.com>
2026-01-10 19:28:53 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,178 @@
+const TELEGRAM_USERNAME="Generation_Z_off";
+
+const products = [
+  {
+    "id": 1,
+    "name": "Caliplates Frozen 🧊🇺🇸",
+    "category": "hash usa",
+    "image": "https://i.postimg.cc/pTdLPwX2/IMG-0662.jpg",
+    "video": "https://files.catbox.moe/d0wcvc.mp4",
+    "description": "NewDrop 2K26 !",
+    "varieties": [
+      "Triangle Kush",
+      "Alien Mintz",
+      "Garlic Budder",
+      "Sherble",
+      "White Runtz",
+      "Starburst OG",
+      "Kush Mintz",
+      "Rainbow Sherbet"
+    ],
+    "prices": [
+      {
+        "amount": "160€",
+        "quantity": "5g"
+      },
+      {
+        "amount": "300€",
+        "quantity": "10g"
+      },
+      {
+        "amount": "750€",
+        "quantity": "25g"
+      },
+      {
+        "amount": "1350€",
+        "quantity": "50g"
+      },
+      {
+        "amount": "2200€",
+        "quantity": "100g"
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "Rubis Farm Frozen 🧊",
+    "category": "hash",
+    "image": "https://i.postimg.cc/RFNQsbcQ/rubisfarm.png",
+    "video": "",
+    "description": "Drop 2K25/2K26 ! On vous a mis des prix de folie !",
+    "varieties": [
+      "Mochilato",
+      "Orange Crush"
+    ],
+    "prices": [
+      {
+        "amount": "120€",
+        "quantity": "10g"
+      },
+      {
+        "amount": "240€",
+        "quantity": "25g"
+      },
+      {
+        "amount": "430€",
+        "quantity": "50g"
+      },
+      {
+        "amount": "750€",
+        "quantity": "100g"
+      }
+    ]
+  },
+  {
+    "id": 3,
+    "name": "Popeye Static ⚡️🇺🇸",
+    "category": "hash usa",
+    "image": "https://i.postimg.cc/y8xqP7D0/popeye.png",
+    "video": "https://files.catbox.moe/ygdn8b.mp4",
+    "description": "NewDrop 2K26 ! Très peu de quantité ...",
+    "varieties": [
+      "Papaya"
+    ],
+    "prices": [
+      {
+        "amount": "180€",
+        "quantity": "5g"
+      },
+      {
+        "amount": "320€",
+        "quantity": "10g"
+      },
+      {
+        "amount": "750€",
+        "quantity": "25g"
+      },
+      {
+        "amount": "1400€",
+        "quantity": "50g"
+      },
+      {
+        "amount": "2400€",
+        "quantity": "100g"
+      }
+    ]
+  },
+  {
+    "id": 4,
+    "name": "Choper Farm Plasma Static ⚡️",
+    "category": "hash",
+    "image": "https://i.postimg.cc/yNhw6x5G/chopper-PNG.png",
+    "video": "https://files.catbox.moe/x4x3o2.mp4",
+    "description": "Drop 2K25/2K26 ! Nouveau drop les amis un putain de static exceptionelle !! ",
+    "varieties": [
+      "Sour X Tropicana Cookies",
+      "Rainbow Sherbet X Skittlez",
+      "Banana X Jealousy",
+      "Grappballs of Fire X",
+      "Milshake X Papayauawg"
+    ],
+    "prices": [
+      {
+        "amount": "100€",
+        "quantity": "5g"
+      },
+      {
+        "amount": "180€",
+        "quantity": "10g"
+      },
+      {
+        "amount": "420€",
+        "quantity": "25g"
+      },
+      {
+        "amount": "750€",
+        "quantity": "50g"
+      },
+      {
+        "amount": "1300€",
+        "quantity": "100g"
+      }
+    ]
+  },
+  {
+    "id": 5,
+    "name": "Weed USA 🍀🇺🇸",
+    "category": "weed usa",
+    "image": "https://i.postimg.cc/pdQH6qBy/Ibigiz.jpg",
+    "video": "https://files.catbox.moe/igluej.mp4",
+    "description": "Drop US 🇺🇸",
+    "varieties": [
+      "Lemon Pinnacle",
+      "Biscotti"
+    ],
+    "prices": [
+      {
+        "amount": "70€",
+        "quantity": "5g"
+      },
+      {
+        "amount": "120€",
+        "quantity": "10g"
+      },
+      {
+        "amount": "230€",
+        "quantity": "25g"
+      },
+      {
+        "amount": "380€",
+        "quantity": "50g"
+      },
+      {
+        "amount": "700€",
+        "quantity": "100g"
+      }
+    ]
+  }
+];
